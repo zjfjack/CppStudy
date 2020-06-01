efef677638e1f1a1021bbaedd31d64c7a57d1691
zjfjack <zekejeff@gmail.com>
2020-06-02 02:42:14 +1000

    Improve ref-qualifier project

--- ReferenceQualifier/ReferenceQualifier.cpp
+++ ReferenceQualifier/ReferenceQualifier.cpp
@@ -6,26 +6,39 @@
 class Foo
 {
 public:
-    Foo(std::vector<int>&& vector) : data(std::move(vector)) {}
+    Foo(std::vector<int>&& vector) : data(std::move(vector)) 
+    {
+        std::cout << "move vector constructor" << std::endl;
+    }
+
+    Foo(Foo&& other) noexcept : data(std::move(other.data))
+    {
+        std::cout << "move constructor" << std::endl;
+    }
+
+    Foo(const Foo& other) : data(other.data)
+    {
+        std::cout << "copy constructor" << std::endl;
+    }
 
     Foo sorted() &&
     {
         std::sort(data.begin(), data.end());
-        return *this;
+        return std::move(*this); // std::move -> Move otherwise it will be copied
     }
 
     Foo sorted() &
     {
         std::sort(data.begin(), data.end());
-        return *this;
+        return *this; // COPY lvalue
     }
 
     // Because of const, we cannot use std::sort which is non-const
     Foo sorted() const &
     {
-        auto temp = *this;
+        auto temp = *this; // copy
         std::sort(temp.data.begin(), temp.data.end());
-        return temp;
+        return temp; // MOVE rvalue (MOVE is elided in release build)
     }
 
     void print(const char* const variableName) const
@@ -42,8 +55,6 @@ public:
 private:
     std::vector<int> data;
 };
-
-
 
 int main()
 {
