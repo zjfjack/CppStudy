ec5d602d7dd95c1dd218f43fe125805ce7e28841
zjfjack <zekejeff@gmail.com>
2020-06-07 23:02:47 +1000

    Add more typedef and using examples in NameSpace project

--- NameSpace/main.cpp
+++ NameSpace/main.cpp
@@ -14,6 +14,9 @@ namespace NameSpace
     };
 }
 
+// namespace alias
+namespace Nested1 = NameSpace::NestedNameSpace1;
+
 NameSpace::NewClass::NewClass(int a, int b) : a(a), b(b) { }
 
 int outScopeVar = 2;
@@ -34,4 +37,24 @@ int main()
     std::string s2 = NameSpace::NestedNameSpace2::func2();
     std::string s3 = NameSpace::InlineNameSpace::func3();
     std::string s4 = NameSpace::func3();
+
+    std::string s5 = Nested1::func1();
+
+    using namespace NameSpace::NestedNameSpace2;
+    std::string s6 = func2();
+    using namespace Nested1;
+    std::string s7 = func1();
+
+    typedef NameSpace::String TypedefNameSpaceString;
+    using UsingNameSpaceString = NameSpace::String;
+    TypedefNameSpaceString s8("test");
+    UsingNameSpaceString s9("test");
+
+    // typedef can defines both pointer and object in one line
+    typedef NameSpace::NewClass TypedefNameSpaceNewClass, *TypedefNameSpaceNewClassPointer;
+    TypedefNameSpaceNewClassPointer p = new TypedefNameSpaceNewClass(5, 3);
+
+    // using can only define one each time
+    using UsingNameSpaceNewClass = NameSpace::NewClass;
+    using UsingNameSpaceNewClassPointer = NameSpace::NewClass*;
 }
